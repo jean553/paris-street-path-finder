e03dcc83e93feab6badda4997407d3e2fbf2526f
jlelievre <jlelievre@rosaly.com>
2021-09-26 19:34:22 +0200

    read the streets polygons file line by line

--- paris-street-path-finder/src/main.rs
+++ paris-street-path-finder/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{
+    BufReader,
+    BufRead,
+};
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Building streets polygons list...");
+
+    const POLYGONS_FILE: &str = "data/streets_polygons";
+    let file = File::open(POLYGONS_FILE).unwrap();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+
+        let json = line.unwrap();
+        println!("{}", json);
+    }
 }
